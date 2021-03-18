3c93e8050d0e612a1bcacb9a64e85b189e2aa77d
Sebastian Laclé <info@slacle.com>
2021-03-18 00:03:26 -0500

    add link to soundcloud

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div className="App">sevaztian.com</div>;
+  return (
+    <div className="App">
+      <h1>sevaztian</h1>
+      <p>
+        <a href="https://soundcloud.com/sevaztian">
+          https://soundcloud.com/sevaztian
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default App;
